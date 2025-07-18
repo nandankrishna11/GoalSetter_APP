413fb9cdd145a2b315a3026fd4dbbd028ed6d0d1
nandankrishna11 <nandankrishna7034@gmail.com>
2025-07-18 18:42:12 +0530

    secoond commit

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,23 @@
+const asyncHandler =require('express-async-handler')
+
+const getGoals =asyncHandler( async(req,res)=>{
+   res.json({message:"Get goals"})
+})
+
+const setGoals = asyncHandler(async(req,res)=>{
+    if (!req.body.text){
+        res.status(400)
+        throw new Error('please add a text field')
+    }
+})
+
+const updateGoals = asyncHandler(async(req,res)=>{
+   res.status(200).json({message:`update goal ${req.params.id}`})
+})
+const deleteGoals = asyncHandler(async(req,res)=>{
+   res.status(200).json({message:`update goal ${req.params.id}`})
+})
+
+
+
+module.exports={getGoals,setGoals,updateGoals,deleteGoals}
